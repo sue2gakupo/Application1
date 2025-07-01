8132f12b69a7308673388ed3258bffc986f044bb
SuuYii <sue2gakupo@gmail.com>
2025-07-01 13:19:50 +0800

    refactor(hw_constraintlayout): Converted specific RelativeLayout usage in hw_constraintlayout to leverage the capabilities of ConstraintLayout

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_hw_linearlayout);
+        setContentView(R.layout.activity_hw_constraintlayout);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
